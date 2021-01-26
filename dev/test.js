60bf81434fe66d356ab3d44ceaabdab6f0a32920
Rishabh <rishabhk2512@gmail.com>
2021-01-26 15:21:34 +0530

    genesis block test

--- dev/test.js
+++ dev/test.js
@@ -1,26 +1,4 @@
 const Blockchain = require('./blockchain');
-
 const bitcoin = new Blockchain();
 
-const previousBlockHash = 'H48RH043W8RHFW8F0UR3';
-const currentBlockData =[
-{
-    sender: 'HE04RHF0834R0W',
-    recipient: 'E3HW0F9RHW904ERH',
-    amount: 1000
-},
-{
-    sender: 'UDSUFUCDSDHF0834R0W',
-    recipient: 'E3HEFDESFEFEWFEW904ERH',
-    amount: 500
-},
-{
-    sender: 'AADSAD834R0W',
-    recipient: 'PGTKOGF9RHW904ERH',
-    amount: 700
-}
-]
-
-const nonce = 100;
-
-console.log(bitcoin.proofOfWork(previousBlockHash,currentBlockData));
+console.log(bitcoin);
