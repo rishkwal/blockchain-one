48d417f94aafd6d02aba046f1050af41c129acae
Rishabh <rishabhk2512@gmail.com>
2021-02-27 12:57:40 +0530

    API Setup

--- dev/api.js
+++ dev/api.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get('/blockchain', function (req, res) {
+});
+
+app.post('./transaction', function(req, res){
+
+});
+
+app.get('./mine', function(req, res){
+
+});
  
-app.listen(3000)
+app.listen(3000, function() {
+  console.log('Listening on port 3000...')
+});
